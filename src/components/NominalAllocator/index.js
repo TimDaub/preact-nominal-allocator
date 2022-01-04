b774f18885f4e8a63f01d8be5ca20528d12b6dcb
Fabian Daubenschuetz <fabian.daubenschuetz@googlemail.com>
2022-01-04 18:18:14 +0100

    added logic for keyboard input min and max range

--- src/components/NominalAllocator/index.js
+++ src/components/NominalAllocator/index.js
@@ -2,10 +2,12 @@
 import { html } from "htm/preact";
 
 function NominalAllocator(props) {
+
   const increment = () => {
     if (props.value < props.max) {
       props.onUpdate(props.value +1);
     }
+
   };
 
   const decrement = () => {
@@ -14,10 +16,19 @@ function NominalAllocator(props) {
     }
   };
 
+  const rangeCheck = (event) => {
+    if (event.target.value > props.max) {
+      props.onUpdate(12);
+    }
+    if (event.target.value < props.min) {
+      props.onUpdate(0);
+    }
+  };
+
   return html`
       <div>
         <button type="button" onClick="${() => decrement()}">-</button>
-        <input type="number" min="${props.min}" max="${props.max}" value=${props.value} />
+        <input type="number" min="${props.min}" max="${props.max}" value=${props.value} onkeyup="${(e) => rangeCheck(e)}"/>
         <button type="button" onClick="${() => increment()}">+</button>
       </div>`
 }
