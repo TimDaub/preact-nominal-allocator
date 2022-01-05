67a5e81954841591631922417f5b758e9380914a
Fabian Daubenschuetz <fabian.daubenschuetz@googlemail.com>
2022-01-05 17:31:05 +0100

    added JSS function to serve css attributes to the NominalAllocator

--- src/components/NominalAllocator/index.js
+++ src/components/NominalAllocator/index.js
@@ -2,10 +2,12 @@
 import { html } from "htm/preact";
 
 function NominalAllocator(props) {
+
   const increment = () => {
     if (props.value < props.max) {
       props.onUpdate(props.value +1);
     }
+
   };
 
   const decrement = () => {
